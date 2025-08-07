be91e2a092fad461282a382581e11d9a52704e44
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-07 10:42:46 +0100

    add-even-number

---
+++ even-only.js
@@ -0,0 +1,20 @@
+function evenNumberonly(numbers){
+    const even = [];
+//     এখানে তুমি একটি খালি array বানালে যার নাম even
+// এটাতে পরে আমরা শুধু even number গুলো রাখবো
+    for(const math of numbers){
+        if (math % 2 ===0) {
+            console.log(math);
+            // console.log(math);  ওই number টাকে console এ দেখাও
+            even.push(math); // even array-তে ওই সংখ্যা যোগ করো
+        }
+    }
+    return even
+}
+const numberOne = [12,23,34,54];
+const evens = evenNumberonly(numberOne);
+console.log(evens);
+
+
+// +++++++++++++++odd number
+
